ef8d92e7f59e791826ad63fbb411074ec692449d
serioga <github@sergey.trofimov.name>
2019-02-15 19:44:24 +0100

    share tests between implementations

--- test/protocol_testing/storage_fake_test.clj
+++ test/protocol_testing/storage_fake_test.clj
@@ -1,75 +1,12 @@
 (ns protocol-testing.storage-fake-test
   (:require
-   [clojure.test :as t]
-   [protocol-testing.storage :as storage]
-   [protocol-testing.storage-fake :as sut]))
+   [protocol-testing.storage-fake :as sut]
+   [protocol-testing.storage-test :as storage-test]))
 
-(t/use-fixtures :each
-  (fn [f]
-    (with-bindings (sut/binding-map (sut/build-db))
-      (f))))
+(defn test-ns-hook []
+  (storage-test/test-suite
+    (fn [f]
+      (println "fixtures: fake")
+      (with-bindings (sut/binding-map (sut/build-db))
+        (f)))))
 
-(defn- build-entity [counter]
-  {:id 42
-   :counter counter})
-
-(t/deftest create
-  (let [test (storage/tx-create (build-entity 0))
-        id (:id test)]
-    (t/is (some? test))
-    (t/is (some? (storage/tx-get-one id)))))
-
-(t/deftest change
-  (let [test (storage/tx-create (build-entity 0))
-        id   (:id test)
-        _    (storage/tx-alter test update :counter inc)
-        test (storage/tx-get-one id)]
-    (t/is (= 1 (:counter test)))))
-
-(t/deftest identity-map-persisted
-  (let [test (storage/tx-create (build-entity 0))
-        id   (:id test)]
-    (storage/with-tx t
-      (let [x (storage/get-one t id)
-            y (storage/get-one t id)]
-        (t/is (identical? x y))))))
-
-(t/deftest identity-map-in-memory
-  (storage/with-tx t
-    (let [x (storage/create t (build-entity 0))
-          y (storage/get-one t (:id @x))]
-      (t/is (identical? x y)))))
-
-(t/deftest identity-map-swap
-  (storage/with-tx t
-    (let [x (storage/create t (build-entity 0))
-          y (storage/get-one t (:id @x))
-          _ (dosync (alter x update :counter inc))]
-      (t/is (= 1 (:counter @x) (:counter @y))))))
-
-(t/deftest concurrency
-  (let [test (storage/tx-create (build-entity 0))
-        id   (:id test)
-        n    10
-        _    (->> (repeatedly #(future (storage/tx-alter test update :counter inc)))
-                  (take n)
-                  (doall)
-                  (map deref)
-                  (doall))
-        test (storage/tx-get-one id)]
-    (t/is (= n (:counter test)))))
-
-(t/deftest inner-concurrency
-  (let [test (storage/tx-create (build-entity 0))
-        id   (:id test)
-        n    10
-        _    (storage/with-tx t
-               (->> (repeatedly #(future (as-> id <>
-                                           (storage/get-one t <>)
-                                           (dosync (alter <> update :counter inc)))))
-                    (take n)
-                    (doall)
-                    (map deref)
-                    (doall)))
-        test (storage/tx-get-one id)]
-    (t/is (= n (:counter test)))))
